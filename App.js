a857d1f8116b1d577929c8d1a31eac060e5e7e48
Babasoylegar <bryanevans890@gmail.com>
2026-07-29 22:12:03 -0300

    Correções

--- App.js
+++ App.js
@@ -1,35 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
 
-// You can import supported modules from npm
-import { Card } from 'react-native-paper';
-
-// or any files within the Snack
-import AssetExample from './components/AssetExample';
-
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
+    <View style ={styles.container}>
+    <Text style ={styles.titulo}>Meu primeiro App React Native</Text>
+    <TextInput style ={styles.campo} placeholder="Digite algo..."/>
+    <View style={styles.row}>
+    <TouchableOpacity style={styles.botao}>
+    <Text>Entrar</Text>
+    </TouchableOpacity>
+    <TouchableOpacity style={styles.botao}>
+    <Text>Sair</Text>
+    </TouchableOpacity>
     </View>
-  );
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    alignItems: 'center',
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
+  row: {
+    flexDirection: 'row', // Coloca os itens lado a lado
+    justifyContent: 'space-between', // Espaço entre os botões
+  },
+  botao: {
+    backgroundColor: "cyan",
+    margin: 15,
+    padding: 15,
+    borderRadius: 15,
+    alignItems: "center",
+    width: 100,
+    height: 50,
+    borderStyle: "#1b3432"
+  },
+
+  titulo: {
+    fontSize: 20,
+    color: "blue",
+    margin: 10,
     fontWeight: 'bold',
-    textAlign: 'center',
   },
-});
+  campo: {
+    height: 25,
+    borderWidth: 1,         // Grossura da borda
+    borderColor: 'black',    // Cor da borda
+    borderRadius: 8,        // Cantos arredondados
+    paddingHorizontal: 10,  // Espaço interno lateral
+  }
+})
